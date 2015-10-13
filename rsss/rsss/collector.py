3d0e79e7c24084778c9ad36995aea03e567b8b0a
gragas <tdf.tomfischer@gmail.com>
2015-10-13 18:24:08 -0500

    Added test.py for debugging (the file should be renamed, so as not to confuse it with a file that would determine whether the installation of this package was successfull or not). Modified collector.py to display basic functionality. I also added modifiable default values for start_date and end_date arguments of the get_geonet_marks function.

--- rsss/rsss/collector.py
+++ rsss/rsss/collector.py
@@ -1,9 +1,18 @@
 import requests
+from xml.etree import ElementTree
 
 class Collector:
+
+    DEFAULT_GEONET_MARK_START_DATE = "1900-01-01" # YYYY-MM-DD
+    DEFAULT_GEONET_MARK_END_DATE   = "2100-12-31"
+
     @staticmethod
-    def getGeoNetMarks(mark_type, status, start_date, end_date):
+    def get_geonet_marks(mark_type, status, start_date=None, end_date=None):
         ## GeoNet Website: http://geonet.org.nz/
+        
+        start_date = start_date if start_date is not None else Collector.DEFAULT_GEONET_MARK_START_DATE
+        end_date = end_date if end_date is not None else Collector.DEFAULT_GEONET_MARK_END_DATE
+
         payload = {
             "type": mark_type,
             "status": status,
@@ -12,5 +21,7 @@ class Collector:
         }
         ## by ommitting the outputFormat argument, the output format
         # will be XML
+        
         URL = "http://magma.geonet.org.nz/ws-delta/site"
         response = requests.get(URL, params=payload)
+        print(response.text)
---
+++ rsss/rsss/test.py
@@ -0,0 +1,7 @@
+from rsss.collector import Collector
+
+def main():
+    Collector.get_geonet_marks("tsunami", "operational") 
+
+if __name__ == "__main__":
+    main()
